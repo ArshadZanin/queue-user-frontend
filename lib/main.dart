3697a765ca302f91f11ef86d45c1067f514a7bcf
ArshadZanin <88363674+ArshadZanin@users.noreply.github.com>
2021-10-28 22:01:40 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:queue/Login/SignUp/create_account.dart';
 
+///main///
 void main() {
   runApp(const MyApp());
 }
